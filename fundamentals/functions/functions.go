3cba7d7c753d7583b6f57cae1ef0f59472fe09ad
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-08-29 00:17:30 -0300

    add examples to function

--- fundamentals/functions/functions.go
+++ fundamentals/functions/functions.go
@@ -2,6 +2,16 @@ package functions
 
 import "fmt"
 
+func sum(a int, b int) int {
+	return a + b
+}
+
+func print(a int, b int) {
+	fmt.Println(sum(a, b))
+}
+
 func Setup() {
 	fmt.Println("Functions!")
+
+	print(2, 3)
 }
