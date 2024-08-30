37a3c18773b7de5684b132bacc796f5c1d076642
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-08-30 01:44:40 -0300

    add examples

--- fundamentals/arithmeticoperators/arithmeticoperators.go
+++ fundamentals/arithmeticoperators/arithmeticoperators.go
@@ -1,7 +1,32 @@
 package arithmeticoperators
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Setup() {
 	fmt.Println("Arithmetic Operators!")
+
+	a := 3
+	b := 2
+
+	fmt.Println("Sum =>", a+b)
+	fmt.Println("Subtraction =>", a-b)
+	fmt.Println("Division =>", a/b)
+	fmt.Println("Multiplication =>", a*b)
+	fmt.Println("Modulus =>", a%b)
+
+	// bitwise
+	fmt.Println("And =>", a&b) // 11 & 10 = 10
+	fmt.Println("Or =>", a|b)  // 11 | 10 = 11
+	fmt.Println("Xor =>", a^b) // 11 ^ 10 = 01
+
+	// other operations using math
+	c := 3.0
+	d := 2.0
+
+	fmt.Println("Max =>", math.Max(float64(a), float64(b)))
+	fmt.Println("Min =>", math.Min(c, d))
+	fmt.Println("Exponentiation =>", math.Pow(c, d))
 }
