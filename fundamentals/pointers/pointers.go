ec81cdb4377731f88eb07be82d0d1fe84d0ca6f5
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-08-30 03:00:41 -0300

    added pointers examples

--- fundamentals/pointers/pointers.go
+++ fundamentals/pointers/pointers.go
@@ -4,4 +4,17 @@ import "fmt"
 
 func Setup() {
 	fmt.Println("POINTERS!")
+
+	i := 1
+
+	// Go does not have pointer arithmetic
+	var p *int = nil
+
+	p = &i
+
+	fmt.Println("Pointer p:", *p)
+
+	*p++
+
+	fmt.Println("Pointer p:", *p)
 }
