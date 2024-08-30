7172a943b345a2a96ad2bd1bae3436712785a607
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-08-30 02:52:41 -0300

    added example to no ternary

--- fundamentals/noternary/noternary.go
+++ fundamentals/noternary/noternary.go
@@ -2,6 +2,18 @@ package noternary
 
 import "fmt"
 
+func getResults(grade float64) string {
+	// return grade >= 6 ? "Approved" : "Disapproved"
+	if grade >= 6 {
+		return "Approved"
+	}
+
+	return "Disapproved"
+}
+
 func Setup() {
 	fmt.Println("GO DOES NOT HAVE TERNARIES")
+
+	fmt.Println(getResults(6.2))
+	fmt.Println(getResults(4.2))
 }
