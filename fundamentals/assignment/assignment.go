f2424d6c0eafbe810da7b2f532aed1471a36d467
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-08-30 01:51:22 -0300

    add examples to assignment

--- fundamentals/assignment/assignment.go
+++ fundamentals/assignment/assignment.go
@@ -4,4 +4,31 @@ import "fmt"
 
 func Setup() {
 	fmt.Println("Assignment!")
+
+	var b byte = 3
+	fmt.Println(b)
+
+	i := 3 // type inference
+	fmt.Println(i)
+
+	i += 3 // i = i + 3
+	fmt.Println(i)
+
+	i -= 3 // i = i - 2
+	fmt.Println(i)
+
+	i /= 2 // i = i / 2
+	fmt.Println(i)
+
+	i *= 2 // i = i * 2
+	fmt.Println(i)
+
+	i %= 2 // i = i % 2
+	fmt.Println(i)
+
+	x, y := 1, 2
+	fmt.Println(x, y)
+
+	x, y = y, x
+	fmt.Println(x, y)
 }
