e3d126ee6d014b28907acdbe4ff6724b4dc9e783
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-08-30 23:26:20 -0300

    added other example to ifelse

--- controlstructure/ifelse/ifelse.go
+++ controlstructure/ifelse/ifelse.go
@@ -10,9 +10,22 @@ func printResult(grade float64) {
 	}
 }
 
+func printResultOtherValidMethod(grade float64) {
+	value := true
+
+	if grade >= 6 && (value || !value) {
+		fmt.Println("Approved with grade", grade)
+	} else {
+		fmt.Println("Disapproved with grade", grade)
+	}
+}
+
 func Setup() {
 	fmt.Println("IF / ELSE")
 
 	printResult(7.3)
 	printResult(3.0)
+
+	printResultOtherValidMethod(6)
+	printResultOtherValidMethod(4)
 }
