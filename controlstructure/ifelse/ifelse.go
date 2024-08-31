7b457c0f10e95141e3870fe94626c4438a2f90bd
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-08-30 22:10:15 -0300

    added example to ifelse

--- controlstructure/ifelse/ifelse.go
+++ controlstructure/ifelse/ifelse.go
@@ -2,6 +2,17 @@ package ifelse
 
 import "fmt"
 
+func printResult(grade float64) {
+	if grade >= 7 {
+		fmt.Println("Approved with grade", grade)
+	} else {
+		fmt.Println("Disapproved with grade", grade)
+	}
+}
+
 func Setup() {
 	fmt.Println("IF / ELSE")
+
+	printResult(7.3)
+	printResult(3.0)
 }
